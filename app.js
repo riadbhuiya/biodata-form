025fde135b0e55bff0c77b7aa72c924319c28e3e
Riad Bhuiya <riadbhuiya828@gmail.com>
2025-10-26 19:04:08 +0600

    validation

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log('alive')
+
+const input = document.getElementsByTagName('input');
+
+const validate = function() {
+    const array = Array.from(input);
+
+    array.forEach((el) => {
+        console.log(el.name)
+        if (el.value === '') {
+            window.alert(`${el.name} cannot be empty`)
+        }
+    })
+}
+
+console.log(input)
